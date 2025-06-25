92f36c91cb088a8baa32b700799dcbb4a9211837
Anna Eklund <anna.eklund@yahoo.se>
2025-06-25 10:26:54 +0200

    Add project files.

---
+++ StorageApi/Models/Products.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StorageApi.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        public int Price { get; set; }
+
+        [Required]
+        public string Category { get; set; } = null!;
+
+        [Required]
+        public string Shelf { get; set; } = string.Empty;
+
+        public int Count { get; set; }
+
+        public required string Description { get; set; }
+    }
+}
